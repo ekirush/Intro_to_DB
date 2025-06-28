e341e0714c7474c3759e78cbd5f2a08292d1d6d2
ekirush <ekirush966@gmail.com>
2025-06-28 16:23:15 +0300

    Updated SQL file 4

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -3,17 +3,17 @@ USE alx_book_store;
 
 -- Table structure for table `authors`
 
-DROP TABLE IF EXISTS authors;
+DROP TABLE IF EXISTS Authors;
 
-CREATE TABLE authors (
+CREATE TABLE Authors (
   author_id int NOT NULL PRIMARY KEY,
   author_name varchar(215) DEFAULT NULL
 );
 
 -- Table structure for table `books`
-DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS Books;
 
-CREATE TABLE books (
+CREATE TABLE Books (
   book_id int NOT NULL PRIMARY KEY,
   title varchar(130) DEFAULT NULL,
   author_id int DEFAULT NULL,
@@ -22,9 +22,9 @@ CREATE TABLE books (
   FOREIGN KEY (author_id) REFERENCES authors (author_id)
 );
 
-DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS Customers;
 
-CREATE TABLE customers (
+CREATE TABLE Customers (
   customer_id int NOT NULL PRIMARY KEY,
   customer_name varchar(215) DEFAULT NULL,
   email varchar(215) DEFAULT NULL,
@@ -34,9 +34,9 @@ CREATE TABLE customers (
 
 -- Table structure for table `order_details`
 
-DROP TABLE IF EXISTS order_details;
+DROP TABLE IF EXISTS Order_Details;
 
-CREATE TABLE order_details (
+CREATE TABLE Order_Details (
   orderdetailid int NOT NULL PRIMARY KEY,
   order_id int DEFAULT NULL,
   book_id int DEFAULT NULL,
@@ -48,9 +48,9 @@ CREATE TABLE order_details (
 
 -- Table structure for table `orders`
 
-DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS Orders;
 
-CREATE TABLE orders (
+CREATE TABLE Orders (
   order_id int NOT NULL PRIMARY KEY,
   customer_id int DEFAULT NULL,
   order_date date DEFAULT NULL,
