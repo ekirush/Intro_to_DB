2da4beaeac537bd1015d92b54f0dad96a297729a
ekirush <ekirush966@gmail.com>
2025-06-28 22:42:22 +0300

    several tasks Update 1

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,14 @@
 USE alx_book_store;
 
-SELECT * FROM alx_book_store.books;
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA 
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = DATABASE()
+    AND TABLE_NAME = 'books';
